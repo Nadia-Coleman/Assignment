36a0625e6993f21257dff55690c140f84ea29fc0
Nadia Coleman <nadiacoleman292@gmail.com>
2021-12-19 21:44:34 -0500

    string formatting

---
+++ stringformatting.py
@@ -0,0 +1,14 @@
+# This prints out "Hello, Nadia!"
+name = "Nadia"
+print("Hello, %s!" % name)
+# This prints out "Nadia is 15 years old."
+name = "Nadia"
+age = 15
+print("%s is %d years old." % (name, age))
+# This prints out: A list: [1, 2, 3]
+mylist = [1,2,3]
+print("A list: %s" % mylist)
+data = ("Nadia", "Coleman", 53.44)
+format_string = "Hello %s %s. Your current balance is $%s."
+
+print(format_string % data)

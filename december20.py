15748e6c84477c6e5a98197084c9e8d4748adf63
Nadia Coleman <nadiacoleman292@gmail.com>
2021-12-20 14:03:26 -0500

    areas of shapes

--- december20.py
+++ december20.py
@@ -1,28 +1,60 @@
 #!/usr/bin/env python3
 
-def squareArea():
-    #modify this function to return the area of a square
-    return
+def squareArea(side):
+    # modify this function to return the area of a square
+    area = side * side * side * side
+    return area
 
-def circleAreaByDiameter():
-    #modify this function to return the area of a circle given the Diameter
-    return
 
-def equilateralTriangleArea():
-    #modify this function to return the area of an Equilateral Triangle given one side
-    return
+def circleAreaByDiameter(diameter):
+    # modify this function to return the area of a circle given the Diameter
+    area = diameter * 3.14 * diameter / 2 * 2
+    return area
 
-def rectangleArea():
-    #modify this function to return the area of an Equilateral Triangle given one side
-    return
+
+def equilateralTriangleArea(side):
+    # modify this function to return the area of an Equilateral Triangle given one side
+    area = side * side * side
+    return area
+
+
+def rectangleArea(sidea, sideb):
+    # modify this function to return the area of an Equilateral Triangle given one side
+    area = sidea * sideb
+    return area
+
+
+def generalTriangleArea(side):
+    area = 1/2 * side * side
+    return area
+
+
+def squareParameter(side):
+    area = side * side
+    return area
+
+
+def circleCircumferance(radius):
+    area = 3.14 * radius * radius
+    return area
+
+
+def cylinderVolume(radius):
+    area = 2 * 3.14 * radius * radius + 2 * 3.14 * radius * radius
+    return area
+
 
 # Also add generalTriangleArea, squareParameter, circleCircumferance, and cylinder volume.
+
 
 def main():
     print("The area of a square with side = to 4 is: ", squareArea(4))
     print("The area of a circle with diameter = to 4 is: ", circleAreaByDiameter(4))
-    print("The area of an Equilateral Triangle with one side = to 4 is: ", equilateralTriangleArea(4))
-    print("The area of an rectangle with one side = to 4 and another = to 8: ", rectangleArea(8,4))
+    print("The area of an Equilateral Triangle with one side = to 4 is: ",
+          equilateralTriangleArea(4))
+    print("The area of an rectangle with one side = to 4 and another = to 8: ",
+          rectangleArea(8, 4))
+
 
 if __name__ == "__main__":
     main()

cd5dd6e28693facd658f7b7be782778d880aeee5
Bryan Patrick Coleman <totalknowledge@gmail.com>
2021-12-20 13:16:08 -0500

    Your assignment today

---
+++ december20.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def squareArea():
+    #modify this function to return the area of a square
+    return
+
+def circleAreaByDiameter():
+    #modify this function to return the area of a circle given the Diameter
+    return
+
+def equilateralTriangleArea():
+    #modify this function to return the area of an Equilateral Triangle given one side
+    return
+
+def rectangleArea():
+    #modify this function to return the area of an Equilateral Triangle given one side
+    return
+
+# Also add generalTriangleArea, squareParameter, circleCircumferance, and cylinder volume.
+
+def main():
+    print("The area of a square with side = to 4 is: ", squareArea(4))
+    print("The area of a circle with diameter = to 4 is: ", circleAreaByDiameter(4))
+    print("The area of an Equilateral Triangle with one side = to 4 is: ", equilateralTriangleArea(4))
+    print("The area of an rectangle with one side = to 4 and another = to 8: ", rectangleArea(8,4))
+
+if __name__ == "__main__":
+    main()
